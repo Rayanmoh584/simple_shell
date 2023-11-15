481ceaca2603e794c19f9193fab737b87e88767f
Rayanmoh584 <95593157+Rayanmoh584@users.noreply.github.com>
2023-11-14 21:10:59 -1200

    Update main.c

--- header/main.c
+++ header/main.c
@@ -1,22 +1,17 @@
 #include "main.h"
 int main (rz , char **argv)
 {
-    char prompt = "(header) $";
-    char  *lineptr;
-    size_t n=0;
-    
-    (void)rz; (void)argv;
-
+char prompt = "(header) $";
+char  *lineptr;
+size_t n=0;
+(void)rz; (void)argv;
 //trying to add infinite loop
-
-    while (1)
-    {
-    printf ("%s", prompt);
-    getline(&lineptr, &n, stdin);
-    printf("%s\n", lineptr);
-
-    free(lineptr);
-    }
-    
-    return (0);
+while (1)
+{
+printf ("%s", prompt);
+getline(&lineptr, &n, stdin);
+printf("%s\n", lineptr);
+free(lineptr);
+}
+return (0);
 }
