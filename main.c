3999499fd39dd4421eeb7c05767109588944d21f
Rayanmoh584 <95593157+Rayanmoh584@users.noreply.github.com>
2023-11-16 16:05:40 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,44 +1,48 @@
-#include "main.h"
+#define MAX_LINE 80 // The maximum length of a command
 
-#define BUFFER_SIZE 1024
+// A function to handle the SIGINT signal (Ctrl-C)
+void sigint_handler(int sig) {
+  printf("\nYou pressed Ctrl-C. To exit the shell, type \"exit\" or \"quit\".\n");
+}
+
 int main(void) {
-    char *line;
-    size_t len = 0;
+  char line[MAX_LINE]; // The input line
+  char *args[2]; // The command line arguments
+  int status; // The exit status of the child process
+  char *environ[] = {NULL}; // The environment variables
 
-    while (1) {
-        printf("#cisfun$ ");
-        if (getline(&line, &len, stdin) == -1) {
-            if (feof(stdin)) {
-                printf("\n");
-                free(line);
-                exit(EXIT_SUCCESS);
-            } else {
-                perror("getline");
-                free(line);
-                exit(EXIT_FAILURE);
-            }
-        }
+  // Set up the signal handler for SIGINT
+  signal(SIGINT, sigint_handler);
 
-        // remove the newline character at the end
-        line[strcspn(line, "\n")] = '\0';
-
-        pid_t pid = fork();
-
-        if (pid == -1) {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            // Child process
-            if (execlp(line, line, (char *)NULL) == -1) {
-                perror("execlp");
-                exit(EXIT_FAILURE);
-            }
-        } else {
-            // parent process 
-            wait(NULL);
-        }
+  while (1) { // Loop indefinitely
+    printf("$ "); // Print the prompt
+    fflush(stdout); // Flush the output buffer
+    if (fgets(line, MAX_LINE, stdin) == NULL) { // If the user entered EOF (Ctrl-D), exit the program
+      printf("\n");
+      break;
     }
-
-    free(line);
-    return 0;
+    line[strcspn(line, "\n")] = '\0'; // Remove the trailing newline from the input
+    if (strcmp(line, "exit") == 0 || strcmp(line, "quit") == 0) { // If the user entered "exit" or "quit", exit the program
+      break;
+    } else if (line[0] == '\0') { // If the user entered an empty line, do nothing
+      continue;
+    } else { // Otherwise, execute the command
+      args[0] = line; // Store the command in the first argument
+      args[1] = NULL; // Terminate the argument array with a null pointer
+      pid_t pid = fork(); // Fork a child process
+      if (pid < 0) { // If the fork failed, print an error message and exit
+        perror("Fork failed");
+        exit(1);
+      } else if (pid == 0) { // If this is the child process, execute the command using execve
+        if (execve(args[0], args, environ) < 0) { // If the execve failed, print an error message and exit
+          perror("Execve failed");
+          exit(1);
+        }
+      } else { // If this is the parent process, wait for the child to terminate and return the exit status
+        waitpid(pid, &status, 0);
+        printf("Child process exited with status %d\n", WEXITSTATUS(status));
+      }
+    }
+  }
+  return 0;
 }
